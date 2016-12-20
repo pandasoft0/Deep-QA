762ecdd1c4d957a95015c102d3b401fd075c05e6
Pandasoft <pandasoft0@gmail.com>
2016-12-20 22:28:24 +0100

    Added support for initializing embeddings from pre-trained word vectors.

--- chatbot/textdata.py
+++ chatbot/textdata.py
@@ -26,7 +26,7 @@ import os  # Checking file existance
 import random
 
 from chatbot.cornelldata import CornellData
-from chatbot.opensubsdata import OpensubsData
+
 
 class Batch:
     """Struct containing batches info
@@ -52,7 +52,7 @@ class TextData:
         self.args = args
 
         # Path variables
-        self.corpusDir = os.path.join(self.args.rootDir, 'data', self.args.corpus)
+        self.corpusDir = os.path.join(self.args.rootDir, 'data/cornell/')
         self.samplesDir = os.path.join(self.args.rootDir, 'data/samples/')
         self.samplesName = self._constructName()
 
@@ -211,12 +211,8 @@ class TextData:
         if not datasetExist:  # First time we load the database: creating all files
             print('Training samples not found. Creating dataset...')
             # Corpus creation
-            if self.args.corpus == 'cornell':
-                cornellData = CornellData(self.corpusDir)
-                self.createCorpus(cornellData.getConversations())
-            elif self.args.corpus == 'opensubs':
-                opensubsData = OpensubsData(self.corpusDir)
-                self.createCorpus(opensubsData.getConversations())
+            cornellData = CornellData(self.corpusDir)
+            self.createCorpus(cornellData.getConversations())
 
             # Saving
             print('Saving dataset...')
